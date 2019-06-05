b6f7b4022feb71b6f415cf813a6d39d46f2dee6f
Gabriel Arguello Costta <gio666nb@gmail.com>
2019-06-05 01:39:26 -0500

    Adaptaciones para seccion Docente

--- Controllers/DocenteController.cs
+++ Controllers/DocenteController.cs
@@ -6,7 +6,13 @@ namespace GestionEscolar.Controllers
     {
         public IActionResult Index()
         {
-            
+            ViewBag.Nombre = "Gabriel";
+            ViewBag.Materia = "Matematica Fiananciera";
+            ViewBag.nr = 24;
+            return View(ViewBag);
+        }
+        public IActionResult Notas()
+        {
             return View();
         }
     }
